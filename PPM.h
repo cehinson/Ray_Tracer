aab0b492ed776c183655952e6cf1bcc67f9eaf62
Charles Hinson <cehinso17@gmail.com>
2018-09-02 10:46:13 +0800

    Clang formated

--- PPM.h
+++ PPM.h
@@ -8,19 +8,21 @@
 #include "Core/Vec3.h"
 #include "Core/Camera.h"
 
-namespace ch {
+namespace ch
+{
 
-	// Writes PPM files
-	class PPM
-	{
-		std::string filename;
-		const size_t MAX_COLOR = 255;
-		const size_t num_rows, num_cols;
+// Writes PPM files
+class PPM
+{
+	std::string filename;
+	const size_t MAX_COLOR = 255;
+	const size_t num_rows, num_cols;
 
-	public:
-		PPM() noexcept;
-		PPM(const std::string& fn, const size_t& cols, const size_t& rows);
-		void write(Vec3<>* pixels[]);
-	};
+  public:
+	PPM()
+	noexcept;
+	PPM(const std::string &fn, const size_t &cols, const size_t &rows);
+	void write(Vec3<> *pixels[]);
+};
 
 } // namespace ch
